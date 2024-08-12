d82e304d4b73e0ae7665f4a397c07afea7b752fd
Sameer Ali <sameersheikh0288@gmail.com>
2024-08-12 12:11:46 +0530

    frontend

--- frontend/src/api/bannerApi.js
+++ frontend/src/api/bannerApi.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://dynamic-banner-page-18gz.vercel.app/api';
 
 const getBanner = async () => {
     const response = await fetch(`${API_URL}/banner`);
